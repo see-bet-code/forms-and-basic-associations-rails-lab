3880c75e65be8ff3cf1eefa8ee024eb0a906f752
see-bet-code <brittani.elan.taylor@gmail.com>
2020-10-09 18:01:49 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,23 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name if self.artist
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      if content.strip != ""
+        note = Note.find_or_create_by(content: content) 
+        self.notes << note
+      end
+    end
+  end
 end
